9863477b933cec10538c30a7d60bb7132d5c26b6
Piotr Banasik <piotr@jane.app>
2019-10-11 10:02:29 -0700

    Update things to 1. no longer use an ENV parser for parsing the Procfile .. it doesn't really make sense, 2. support env setting and env replacement in the commands

--- main.go
+++ main.go
@@ -3,32 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"reflect"
+	"regexp"
+	"strings"
 	"syscall"
 
-	"github.com/subosito/gotenv"
+	shellwords "github.com/mattn/go-shellwords"
 )
 
 // Config type based on what is in the Procfile
 type Config map[string]string
 
-// ReadConfig from https://github.com/ddollar/forego
-func ReadConfig(filename string) (Config, error) {
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		return make(Config), nil
-	}
-	fd, err := os.Open(filename)
+// read Procfile and parse it.
+func readProcfile(procfile string) (Config, error) {
+	content, err := ioutil.ReadFile(procfile)
 	if err != nil {
 		return nil, err
 	}
-	defer fd.Close()
-	config := make(Config)
-	for key, val := range gotenv.Parse(fd) {
-		config[key] = val
+
+	procs := Config{}
+
+	for _, line := range strings.Split(string(content), "\n") {
+		tokens := strings.SplitN(line, ":", 2)
+		if len(tokens) != 2 || tokens[0][0] == '#' {
+			continue
+		}
+		k, v := strings.TrimSpace(tokens[0]), strings.TrimSpace(tokens[1])
+		procs[k] = v
 	}
-	return config, nil
+
+	return procs, nil
 }
 
 func commandPrep(parts ...string) *exec.Cmd {
@@ -37,10 +44,35 @@ func commandPrep(parts ...string) *exec.Cmd {
 	return exec.Command(command, remainingParts...)
 }
 
+func envFromCmd(cmd string) ([]string, string, error) {
+	words, err := shellwords.Parse(cmd)
+	if err != nil {
+		return nil, cmd, fmt.Errorf("Failed parsing command words: %v", err)
+	}
+
+	done := false
+	result := []string{}
+
+	for _, word := range words {
+		if !done {
+			match, _ := regexp.MatchString("^[^=]+=[^=]+$", word)
+			if match {
+				result = append(result, word)
+			} else {
+				done = true
+			}
+		}
+	}
+
+	words = words[len(result):len(words)]
+
+	return result, strings.Join(words, " "), nil
+}
+
 func main() {
 	flag.Parse()
 
-	config, error := ReadConfig("Procfile")
+	config, error := readProcfile("Procfile")
 	if error != nil {
 		fmt.Fprintf(os.Stderr, "[forerun] Failed reading Procfile: %v\n", error)
 		os.Exit(1)
@@ -60,10 +92,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	words := []string{"/bin/bash", "-c", fmt.Sprintf("exec %s", cmdString)}
+	additionalEnv, newCmdString, addEnvErr := envFromCmd(cmdString)
+	if addEnvErr != nil {
+		fmt.Fprintf(os.Stderr, "Failed getting additional env: %v", addEnvErr)
+		os.Exit(1)
+	}
+
+	words := []string{"/bin/bash", "-c", fmt.Sprintf("exec %s", newCmdString)}
 
 	cmd := commandPrep(words...)
+	env := append(os.Environ(), additionalEnv...)
+
 	fmt.Printf("[forerun] Running command: %v ...\n", words)
+	if len(additionalEnv) > 0 {
+		fmt.Printf("[forerun] Additional env: %v\n", additionalEnv)
+	}
+
+	cmd.Env = env
 
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
