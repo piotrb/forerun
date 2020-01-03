59482d75bd2afff791d049f842bebed1fad2da01
Piotr Banasik <piotr@jane.app>
2020-01-03 14:13:40 -0800

    Tweak exit code handling, treat "signal" exits as "ok" and just print extra debug info

--- utils.go
+++ utils.go
@@ -26,14 +26,13 @@ func statusFromCmd(cmd *exec.Cmd) (*syscall.WaitStatus, error) {
 	if cmd.ProcessState != nil {
 		status := cmd.ProcessState.Sys().(syscall.WaitStatus)
 		return &status, nil
-	} else {
-		return nil, errors.New("no cmd ProcessState")
 	}
+	return nil, errors.New("no cmd ProcessState")
 }
 
 func handleCmdExit(cmd *exec.Cmd, err error, prefix string) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%sCommand failed - %v\n", prefix, err)
+		fmt.Fprintf(os.Stderr, "%sCommand Run Failed - %v\n", prefix, err)
 	}
 
 	var statusErr error = nil
@@ -41,9 +40,22 @@ func handleCmdExit(cmd *exec.Cmd, err error, prefix string) {
 		var status *syscall.WaitStatus = nil
 		status, statusErr = statusFromCmd(cmd)
 		if statusErr != nil {
-			fmt.Printf("%sFailed Getting Status: %v", prefix, statusErr)
+			fmt.Printf("%sFailed Getting Status: %v\n", prefix, statusErr)
 		} else {
-			os.Exit(status.ExitStatus())
+			if status.ExitStatus() > -1 {
+				os.Exit(status.ExitStatus())
+			} else if status.Signal() > -1 {
+				fmt.Printf("%sexited with signal: %v\n", prefix, status.Signal())
+				os.Exit(0)
+			} else {
+				fmt.Printf("%sstatus exited: %v\n", prefix, status.Exited())
+				fmt.Printf("%sstatus exit status: %v\n", prefix, status.ExitStatus())
+				fmt.Printf("%sexited with signal: %v\n", prefix, status.Signal())
+				fmt.Printf("%sstatus stop signal: %v\n", prefix, status.StopSignal())
+				fmt.Printf("%sstatus stop signal: %v\n", prefix, status.StopSignal())
+				fmt.Printf("%sstatus trap cause: %v\n", prefix, status.TrapCause())
+				os.Exit(-1)
+			}
 		}
 	}
 
